478645ed83409923491e2a7814cafb93cebdf713
Roman Erzhukov <ergukov@i-teco.ru>
2021-10-10 11:00:19 +0300

    Document <-> DocumentDto: success

--- src/main/java/mapper/DocumentMapper.java
+++ src/main/java/mapper/DocumentMapper.java
@@ -1,6 +1,8 @@
 package mapper;
 
 import dto.DocumentDto;
+import dto.DocumentStatus;
+import dto.StatusDto;
 import entity.Document;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,8 +14,29 @@ import org.mapstruct.Mapping;
  */
 @Mapper
 public abstract class DocumentMapper {
-    @Mapping(target = "status.system", source = "status")
     public abstract DocumentDto toDto(Document entity);
+
+    StatusDto mapStatus(DocumentStatus status) {
+        if (status == null) {
+            return null;
+        }
+        return new StatusDto()
+                .setSystem(status)
+                .setName(statusName(status));
+    }
+
+    private String statusName(DocumentStatus status) {
+        switch (status) {
+            case NEW:
+                return "Новый";
+            case DRAFT:
+                return "Черновик";
+            case SIGNED:
+                return "Подписан";
+            default:
+                return "Исполнен";
+        }
+    }
 
     @Mapping(target = "status", source = "status.system")
     public abstract Document toEntity(DocumentDto dto);
